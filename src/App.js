e9e05df7a85e2fec0b429aa502305a9a5d3381c7
sajith.thomas <sajith.thomas@ext.idfy.com>
2024-02-25 13:26:05 +0530

    implemented with Howl

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import './App.css';
 import song1 from './audio/incoming-voiceover.mp3'
 import song2 from './audio/ringtone.mp3'
+import {Howl, Howler} from 'howler';
 function App() {
   navigator.mediaDevices.getUserMedia({ audio: { output: true } })
   .then(function(stream) {
-    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    // Permission granted, you can now use the stream to output audio
-var audioElement = new Audio(song1);
-
-// Mute the audio immediately after creating the Audio element
-// audioElement.muted = true;
-
-// Create a source node
-var source = audioContext.createMediaElementSource(audioElement);
-
-// Connect the source to the context's destination (speakers)
-source.connect(audioContext.destination);
-
-// Delay the start of audio playback
-setTimeout(function() {
-  // audioElement.muted = false;
-    audioElement.play();
-}, 100);
-audioElement.onended = handleIncommingVoiceEnd;
+    var sound = new Howl({
+      src: [song1]
+    });
+    
+    // Clear listener after first call.
+    sound.once('load', function(){
+      sound.play();
+    });
+    
+    // Fires when the sound finishes playing.
+    sound.on('end', function(){
+      handleIncommingVoiceEnd();
+    });
   })
   .catch(function(err) {
     // Permission denied or error occurred
@@ -32,28 +27,19 @@ audioElement.onended = handleIncommingVoiceEnd;
 
 
   const handleIncommingVoiceEnd = () => {
-    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    var audioElement2 = new Audio(song2);
-    // Mute the audio immediately after creating the Audio element
-    // audioElement2.muted = true;
-    
-    // Create a source node
-    var source = audioContext.createMediaElementSource(audioElement2);
-    
-    // Connect the source to the context's destination (speakers)
-    source.connect(audioContext.destination);
-    
-    // Delay the start of audio playback
-    setTimeout(function() {
-      // audioElement2.muted = false;
-        audioElement2.play();
-        audioElement2.loop = true;
-    }, 100);
+    var sound2 = new Howl({
+      loop: true,
+      src: [song2]
+    });
+    // Clear listener after first call.
+    sound2.once('load', function(){
+      sound2.play();
+    });
   }
   return (
     <div className="App">
-      v.3
-    </div>
+          v.6
+      </div>
   );
 }
 
