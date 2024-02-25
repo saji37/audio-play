50153940eb133b3f1f9403c4d20065db6e9a78d9
sajith.thomas <sajith.thomas@ext.idfy.com>
2024-02-25 12:46:56 +0530

    used audioContext to play audio

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import song2 from './audio/ringtone.mp3'
 function App() {
   navigator.mediaDevices.getUserMedia({ audio: { output: true } })
   .then(function(stream) {
+    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
     // Permission granted, you can now use the stream to output audio
-    const audio = new Audio(song1);
-    audio.load();
-    audio.autoplay=true;
-    audio.play=true;
+var audioElement = new Audio(song1);
+
+// Mute the audio immediately after creating the Audio element
+// audioElement.muted = true;
+
+// Create a source node
+var source = audioContext.createMediaElementSource(audioElement);
+
+// Connect the source to the context's destination (speakers)
+source.connect(audioContext.destination);
+
+// Delay the start of audio playback
+setTimeout(function() {
+  // audioElement.muted = false;
+    audioElement.play();
+}, 100);
+audioElement.onended = handleIncommingVoiceEnd;
   })
   .catch(function(err) {
     // Permission denied or error occurred
@@ -17,22 +31,29 @@ function App() {
 
 
 
-  // const handleIncommingVoiceEnd = () => {
-  //   const ringtoneAudioElement = document.getElementById(
-  //     'ringtone',
-  //   );
-  //   if (ringtoneAudioElement) ringtoneAudioElement.play();
-  // }
+  const handleIncommingVoiceEnd = () => {
+    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    var audioElement2 = new Audio(song2);
+    // Mute the audio immediately after creating the Audio element
+    // audioElement2.muted = true;
+    
+    // Create a source node
+    var source = audioContext.createMediaElementSource(audioElement2);
+    
+    // Connect the source to the context's destination (speakers)
+    source.connect(audioContext.destination);
+    
+    // Delay the start of audio playback
+    setTimeout(function() {
+      // audioElement2.muted = false;
+        audioElement2.play();
+        audioElement2.loop = true;
+    }, 100);
+  }
   return (
     <div className="App">
-          {/* <audio
-            src={song1}
-            playsInline
-            autoPlay
-            onEnded={handleIncommingVoiceEnd}
-          />
-          <audio id="ringtone" src={song2} loop playsInline />     */}
-      </div>
+      v.3
+    </div>
   );
 }
 
