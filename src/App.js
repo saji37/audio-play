fe41132f3873e2605572796bae74432004afd613
sajith.thomas <sajith.thomas@ext.idfy.com>
2024-02-25 14:27:15 +0530

    removed delay

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
 import './App.css';
 import song1 from './audio/incoming-voiceover.mp3'
 import song2 from './audio/ringtone.mp3'
-import {Howl, Howler} from 'howler';
+import {Howl} from 'howler';
 function App() {
   navigator.mediaDevices.getUserMedia({ audio: { output: true } })
   .then(function(stream) {
     var sound = new Howl({
-      src: [song1]
+      src: [song1],
+      autoplay: true,
     });
+    var sound2 = new Howl({
+      loop: true,
+      src: [song2]
+    });
+    sound2.pause();
     
     // Clear listener after first call.
     sound.once('load', function(){
       sound.play();
     });
-    
-    // Fires when the sound finishes playing.
     sound.on('end', function(){
-      handleIncommingVoiceEnd();
+      sound2.play();
     });
   })
   .catch(function(err) {
     // Permission denied or error occurred
     console.error('Error accessing speaker:', err);
   });
-
-
-
-  const handleIncommingVoiceEnd = () => {
-    var sound2 = new Howl({
-      loop: true,
-      src: [song2]
-    });
-    // Clear listener after first call.
-    sound2.once('load', function(){
-      sound2.play();
-    });
-  }
   return (
     <div className="App">
           v.6
